c5ddbccfdd2e9655b942a6a445492e704346c361
argun8394 <argundoganali@gmail.com>
2022-01-18 23:39:47 +0300

    fetch_api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import Card from './components/Card';
-import data from './data.json';
+import React, { useState, useEffect } from 'react';
+// import data from './data.json'; //data ile hata veriyor!!!!!!!!!!!!!!!!!
 
 function App() {
+const [itemList, setItemList] = useState([]);
+
+useEffect(() =>{
+fetch('https://www.breakingbadapi.com/api/characters')
+  .then(response => response.json())
+  .then((data) =>setItemList(data))
+},[]
+);
+
+
   return (
     <div className="App">
       <header className="header">
         <img src="https://www.logolynx.com/images/logolynx/55/55a537ca9549600cfd1d0b274b3bc61.png" alt="breaking Bad logo" />
 
       </header>
+      <div className="cards">
+        {itemList.map((item) => (
+
+          <Card key={item.char_id} item={item} />))}
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default App;
